8586d28d9c554dee5f6467d11df372988c085faf
Vo Huy <huyvntse161762@fpt.edu.vn>
2022-11-12 23:54:49 +0700

    edit cookie time

--- user/auth/login.php
+++ user/auth/login.php
@@ -28,9 +28,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } else if ($num == 1) {
         if (!empty($_POST["remember"])) {
             //COOKIES for username
-            setcookie("user_login", $_POST["username"], time() + (10 * 365 * 24 * 60 * 60));
+            setcookie("user_login", $_POST["username"], time() + (86400 * 7));
             //COOKIES for password
-            setcookie("userpassword", $_POST["password"], time() + (10 * 365 * 24 * 60 * 60));
+            setcookie("userpassword", $_POST["password"], time() + (86400 * 7));
         } else {
             if (isset($_COOKIE["user_login"])) {
                 setcookie("user_login", "");
