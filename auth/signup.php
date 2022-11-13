4b1d09116f83011bd47bde780568ec9668d737d5
Vo Huy <huyvntse161762@fpt.edu.vn>
2022-11-14 00:59:44 +0700

    n

--- auth/signup.php
+++ auth/signup.php
@@ -76,7 +76,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1,
 		shrink-to-fit=no">
-
+    <title>Sign up</title>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 </head>
